216b045dc409e0c5ccafb1b23ad9bb6f70cacd4c
johqniel <daniel.nogues.daniel@gmail.com>
2025-10-09 14:56:19 +0200

    Test how this works.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,39 @@
-const message: string = "Hallo TypeScript!";
-console.log(message);
 
-const h1 = document.createElement("h1");
-h1.textContent = message;
-document.body.appendChild(h1);
+// Get DOM elements
+const input = document.getElementById("todo-input") as HTMLInputElement;
+const addBtn = document.getElementById("add-btn") as HTMLButtonElement;
+const todoList = document.getElementById("todo-list") as HTMLUListElement;
 
-console.log("✅ TypeScript läuft!");
-document.body.innerHTML = "<h1>Hallo TypeScript!</h1>";
+// add a new Todo
+
+function addTodo() {
+    const todoTest = input.value.trim();
+    if(!todoTest) return;
+
+    const li = document.createElement("li");
+    li.textContent = todoTest;
+
+    // Toggle "done" class on click
+    li.addEventListener("click", () =>{
+        li.classList.toggle("done");
+    });
+
+    // Remove todo on double click
+    li.addEventListener("dblclick", () => {
+        todoList.removeChild(li);
+    });
+
+    todoList.appendChild(li);
+    input.value = "";
+
+}
+
+// Event listener
+
+addBtn.addEventListener("click", addTodo);
+
+// Add todo on enter key
+
+input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") addTodo();
+});
